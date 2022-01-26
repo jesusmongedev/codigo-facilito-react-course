89ee64f9ee39de4a3dd671963c6ef28572767596
Jesús Monge <jesus_jaml@outlook.com>
2022-01-25 23:29:17 -0500

    Custom Checkbox. Fixing max-width. Next responsive font. Lazy loading. UseLocalStorage SetTimeOut⌚

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,14 +1,11 @@
 import React, { useContext } from 'react'
 import { TodoContext } from '../TodoContext'
-// import FlipMove from 'react-flip-move'
+import FlipMove from 'react-flip-move'
 import './styles.css'
-import { MdDelete, MdModeEdit, MdDragHandle } from 'react-icons/md'
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
-
+import { MdDelete, MdModeEdit } from 'react-icons/md'
 const TodoList = () => {
   const {
     todos,
-    saveTodos,
     deleteTodo,
     toggleCompleteTodo,
     completedTodos,
@@ -18,69 +15,38 @@ const TodoList = () => {
 
   return (
     <div className="todos-container">
-      <DragDropContext
-        onDragEnd={(param) => {
-          const srcI = param.source.index
-          const desI = param.destination?.index
-          if (desI) {
-            todos.splice(desI, 0, todos.splice(srcI, 1)[0])
-            saveTodos(todos)
-          }
-        }}
-      >
-        <h2>
-          {completedTodos} completed of {totalTodos}
-        </h2>
+      <h2>
+        {completedTodos} completed of {totalTodos}
+      </h2>
+      <FlipMove typeName="ul">
         {/* map over the todos array which creates a new li element for every todo */}
-        <Droppable droppableId="droppable-1">
-          {(provided, _) => (
-            <ul
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-              // typeName="ul"
+        {todos.map((todo, i) => (
+          <div className="todo-text" key={i}>
+            <li
+              className={`todo-list ${
+                todo.completed ? 'todo-list--completed' : ''
+              }`}
             >
-              {todos.map((todo, i) => (
-                <Draggable key={i} draggableId={'draggable-' + i} index={i}>
-                  {(provided, snapshot) => (
-                    <div
-                      className="todo-text"
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                    >
-                      <li
-                        className={`todo-list ${
-                          todo.completed ? 'todo-list--completed' : ''
-                        }`}
-                      >
-                        <span {...provided.dragHandleProps}>
-                          <MdDragHandle />
-                        </span>
-                        <input
-                          type="checkbox"
-                          onClick={() => toggleCompleteTodo(todo.text)}
-                          defaultChecked={todo.completed}
-                        />
-                        {todo.text}
-                      </li>
-                      <div className="todo-actions">
-                        <MdModeEdit
-                          className="todo-actions-button"
-                          onClick={() => editTodo(todo)}
-                        />
-                        <MdDelete
-                          className="todo-actions-button"
-                          onClick={() => deleteTodo(todo.text)}
-                        />{' '}
-                      </div>
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </ul>
-          )}
-        </Droppable>
-      </DragDropContext>
+              <input
+                type="checkbox"
+                onClick={() => toggleCompleteTodo(todo.text)}
+                defaultChecked={todo.completed}
+              />
+              {todo.text}
+            </li>
+            <div className="todo-actions">
+              <MdModeEdit
+                className="todo-actions-button edit"
+                onClick={() => editTodo(todo)}
+              />
+              <MdDelete
+                className="todo-actions-button delete"
+                onClick={() => deleteTodo(todo.text)}
+              />
+            </div>
+          </div>
+        ))}
+      </FlipMove>
     </div>
   )
 }
